29843485adb25f3b7644bc1c9b7addfe04a650d8
Yujian-Fu <yujian_fu@outlook.com>
2020-01-22 01:56:15 +0800

    all

--- faiss/faiss_configuration.py
+++ faiss/faiss_configuration.py
@@ -3,7 +3,7 @@ CONFIG = edict()
 
 #directory for evaluation datasets
 CONFIG.DATASET_PATH_LIST = [
-    #'/home/y/yujianfu/similarity_search/datasets/ANN_GIST1M/gist_base.fvecs',
+    '/home/y/yujianfu/similarity_search/datasets/ANN_GIST1M/gist_base.fvecs',
     '/home/y/yujianfu/similarity_search/datasets/ANN_SIFT1M/sift_base.fvecs',
     '/home/y/yujianfu/similarity_search/datasets/ANN_SIFT10K/siftsmall_base.fvecs',
     '/home/y/yujianfu/similarity_search/datasets/Cifar/cifar-10-batches-py/images_train.npy',
@@ -15,7 +15,7 @@ CONFIG.DATASET_PATH_LIST = [
 
 #directory for evaluation queries
 CONFIG.QUERY_PATH_LIST = [
-    #'/home/y/yujianfu/similarity_search/datasets/ANN_GIST1M/gist_query.fvecs',
+    '/home/y/yujianfu/similarity_search/datasets/ANN_GIST1M/gist_query.fvecs',
     '/home/y/yujianfu/similarity_search/datasets/ANN_SIFT1M/sift_query.fvecs',
     '/home/y/yujianfu/similarity_search/datasets/ANN_SIFT10K/siftsmall_query.fvecs',
     '/home/y/yujianfu/similarity_search/datasets/Cifar/cifar-10-batches-py/images_train_query.npy',
@@ -27,7 +27,7 @@ CONFIG.QUERY_PATH_LIST = [
 
 #path to evaluation train dataset
 CONFIG.TRAIN_PATH_LIST = [
-    #'/home/y/yujianfu/similarity_search/datasets/ANN_GIST1M/gist_learn.fvecs',
+    '/home/y/yujianfu/similarity_search/datasets/ANN_GIST1M/gist_learn.fvecs',
     '/home/y/yujianfu/similarity_search/datasets/ANN_SIFT1M/sift_learn.fvecs',
     '/home/y/yujianfu/similarity_search/datasets/ANN_SIFT10K/siftsmall_learn.fvecs',
     '/home/y/yujianfu/similarity_search/datasets/Cifar/cifar-10-batches-py/images_train_learn.npy',
