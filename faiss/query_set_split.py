10c0d5bec0dce1e4b557d08da6bc36ee62567708
Yujian <yujian_fu@outlook.com>
2020-02-01 03:23:48 +0800

    all

--- faiss/query_set_split.py
+++ faiss/query_set_split.py
@@ -1,5 +1,7 @@
 import numpy as np 
 import os
+from fvecs_read import fvecs_read
+import sys
 
 def distance_computing(query_point, search_dataset):
     (instances, dimension) = search_dataset.shape
@@ -9,22 +11,40 @@ def distance_computing(query_point, search_dataset):
     return np.sqrt(a + b - 2*c)
 
 
+def read_dataset(file_name):
+    if file_name.split('.')[-1] == 'npy':
+        file = np.load(file_name)
+    elif file_name.split('.')[-1] == 'fvecs':
+        file = fvecs_read(file_name)
+    else:
+        print ('the file name', file_name, 'is wrong!')
+
+    return file
 
 
+start_num = sys.argv[1:]
+start_num = int(start_num[0])
 
 dataset_list = [
     '/home/y/yujianfu/similarity_search/datasets/ANN_SIFT10K/SIFT10K_base.npy', 
     '/home/y/yujianfu/similarity_search/datasets/ANN_SIFT1M/SIFT1M_base.npy',
+    '/home/y/yujianfu/similarity_search/datasets/ANN_GIST1M/GIST1M_base.npy',
+    '/home/y/yujianfu/similarity_search/datasets/cifar/images_train.npy',
+    '/home/y/yujianfu/similarity_search/datasets/deep1M/deep1M_base.npy',
+    '/home/y/yujianfu/similarity_search/datasets/Glove/glove_840_300d.npy',
+    '/home/y/yujianfu/similarity_search/datasets/MNIST/MNIST_train_data.npy',
+    '/home/y/yujianfu/similarity_search/datasets/SIFT10M/SIFT10M_feature.npy'
 ]
 
 
 K = 1000
-for dataset_path in dataset_list:
-    search_dataset = np.load(dataset_path)
+for dataset_path in dataset_list[start_num:start_num+1]:
+    search_dataset = read_dataset(dataset_path)
     record_path = '/'
     for split_part in dataset_path.split('/')[0:-1]:
         record_path = os.path.join(record_path, split_part)
     (instances, dimension) = search_dataset.shape
+    assert instances > dimension
 
     RC = np.zeros((instances, 1))
     LID_MLE_1000 = np.zeros((instances, 1))
