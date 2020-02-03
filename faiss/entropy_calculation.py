23f18390a6d2cf719dee5798ba0efc557e6525a1
Yujian-Fu <yujian_fu@outlook.com>
2020-02-03 15:42:05 +0800

    all

--- faiss/entropy_calculation.py
+++ faiss/entropy_calculation.py
@@ -1,1 +1,13 @@
 import numpy as np 
+
+
+dataset_path_list = [
+
+
+]
+
+dataset = np.load(dataset_path)
+instances, dimension = dataset.shape
+for i in dimension:
+     feature_column = dataset[:, i]
+     max_value = 
