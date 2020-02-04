c88635f42af18ae0558f0a9f0ce6a04a0135dcc7
Yujian-Fu <yujian_fu@outlook.com>
2020-02-04 11:52:17 +0800

    all

--- faiss/test_characters_visualization.py
+++ faiss/test_characters_visualization.py
@@ -18,23 +18,73 @@ dataset_list = [
     'SIFT10K'
 ]
 
+metric_list = [
+    'large_LID',
+    'small_LIC',
+    'large_RC',
+    'small_RC'
+]
+
 record_path = ''
 
 for dataset in dataset_list:
     for model in model_list:
-        file_path = op.path.join(record_path, dataset, model, model+'.txt')
+        file_path = os.path.join(record_path, dataset, model, model+'.txt')
         content = file.read()
         content = content.split('\n')[1:]
         instances = len(content)
+        assert instances % 4 == 0
         performance = np.zeros((instances, 2))
         plt.figure()
+        performance = np.zeros((int(instances/4), 2))
+
         for i in range(int(instances/4)):
             line = content[i].split(' ')
             recall = float(line[-3])
             qps = float(line[-1])
             performance[i, 0] = recall
             performance[i, 1] = qps
-            plt.plot(performance[:,0], performance[:, 1])
-
+            plt.plot(performance[:,0], performance[:, 1], label = 'large_LID')
         
+        for i in range(int(instances/4), int(instances/2)):
+            line = content[i].split(' ')
+            recall = float(line[-3])
+            qps = float(line[-1])
+            performance[i, 0] = recall
+            performance[i, 1] = qps
+            plt.plot(performance[:,0], performance[:, 1], label = 'small_LID')
+
+        for i in range(int(instances/2), int(instances*3/4)):
+            line = content[i].split(' ')
+            recall = float(line[-3])
+            qps = float(line[-1])
+            performance[i, 0] = recall
+            performance[i, 1] = qps
+            plt.plot(performance[:,0], performance[:, 1], label = 'large_RC')
+
+        for i in range(int(instances*3/4), int(instances)):
+            line = content[i].split(' ')
+            recall = float(line[-3])
+            qps = float(line[-1])
+            performance[i, 0] = recall
+            performance[i, 1] = qps
+            plt.plot(performance[:,0], performance[:, 1], label = 'small_RC')
+        
+        plt.title('recall-qps '+dataset+' '+model)
+        plt.legend()
+        plt.savefig(os.path.join(record_path, dataset, model, 'recall-qps'+dataset+'-'+model+'.png'))
+
+        for k in k_list:
+            for metric in metric_list:
+                recall_dis = np.load(record_path, dataset, model,metric+str(k)+'.npy')
+                recall_dis = recall_dis.reshape(recall_dis.shape[0],)
+                sns.kdeplot(recall_dis, shade = 'True', color = 'black')
+                sns.rugplot(recall_dis, color = 'black')
+                axes = plt.gca()
+                y_min, y_max = axes.get_ylim()
+                plt.vlines(np.median(recall_dis), y_min, y_max, color = 'black', linestyles = 'dashed')
+                print('the median is ', np.median(recall_dis))    
+                        
+
+              
 
