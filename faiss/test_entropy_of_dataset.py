56c9c3f4c6f347d244a922b532b52b9018ba2ed4
Yujian <yujian_fu@outlook.com>
2020-02-10 01:30:32 +0800

    all

--- faiss/test_entropy_of_dataset.py
+++ faiss/test_entropy_of_dataset.py
@@ -42,7 +42,8 @@ for dataset_index in range(len(dataset_list)):
             dis_search, ID_search = index.search(dataset[i, :].reshape(1, -1), k)
             time_end = time.time()
             qps = 1 / (time_end - time_start)
-            recall = len(set(id_truth[:, 0:k].reshape(1, )) & set(ID_search.reshape(1, )))/len(set(id_truth[:, 0:k].reshape(1, )))
+            dis_truth, id_truth = index_brute.search(dataset[i, :].reshape(1, -1), k)
+            recall = len(set(id_truth[0,:]) & set(ID_search[0,:]))/len(set(id_truth[0, :]))
             performance[i, 2*j+1] = recall
             performance[i, 2*j+2] = qps
             print('the result now is: ', i, k, recall, qps)
