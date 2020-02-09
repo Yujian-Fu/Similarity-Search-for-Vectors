76d23e0f02e2316772c0d8bc85020ae88cd40718
Yujian <yujian_fu@outlook.com>
2020-02-10 00:54:26 +0800

    all

--- faiss/test_entropy_of_dataset.py
+++ faiss/test_entropy_of_dataset.py
@@ -3,25 +3,51 @@ import os
 import faiss
 
 dataset_list = [
-
-
+    '/home/y/yujianfu/similarity_search/datasets/ANN_SIFT10K/SIFT10K_base.npy', 
 ]
 
-entropy_path = 
+entropy_list = [
+    '/home/y/yujianfu/similarity_search/datasets/ANN_SIFT10K/entropy.npy'
+]
 
-k_list = [1, 5, 10, 50]
+save_path = '/home/y/yujianfu/similarity_search/datasets/'
+k_list = [3, 5, 10, 50]
 
-for dataset_path in dataset_list:
+for dataset_index in len(dataset_list):
+    dataset_path = dataset_list[dataset_index]
+    dataset_name = dataset_path.split('/')[-1].split('_')[0]
+    entropy_path = entropy_list[dataset_index]
+    
     dataset = np.load(dataset_path)
     instances, dimension = dataset.shape
     dataset = np.transpose(dataset)
 
     entropy = np.load(entropy_path)
-    performance = np.zeros(dimension, 2)
+    performance = np.zeros(dimension, 7)
     index_brute = faiss.IndexFlatL2(instances)
     index_brute.add(dataset)
-    for k in k_list:
-        ground_truth = faiss.search
-        
+    quantilizer = faiss.IndexFlatL2(instances)
+    index = faiss.IndexIVFFlat(quantilizer, instances, 5)
+    index.probe = 2
+    index.train(dataset[0:50,:])
+
+    for i in range(dimension):
+        dis_truth, id_truth = index_brute.search(dataset[i], k_list[-1])
+        for j in len(k_list):
+            k = k_list[j]
+            time_start = time.time()
+            dis_search, ID_search = index.search(dataset[i], k)
+            time_end = time.time()
+            qps = 1 / (time_end - time_start)
+            recall = recall_record[j, 0] = len(set(id_truth[:, 0:k]) & set(ID_search))/len(set(id_truth[:, 0:k]))
+            performance[i, 2*j+1] = recall
+            performance[i, 2*j+2] = qps
+    
+    np.save(op.path.join(save_path, dataset_name, 'entropy_performance.npy'),performance)
+
+
+
+         
+
 
 
