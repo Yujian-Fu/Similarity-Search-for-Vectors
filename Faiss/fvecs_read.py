d0a90ed7f2c1a44c67b33858015b1d1a09afdfeb
Yujian <yujian_fu@outlook.com>
2020-02-17 00:22:24 +0800

    all

--- Faiss/fvecs_read.py
+++ Faiss/fvecs_read.py
@@ -24,4 +24,7 @@ def fvecs_read(fname):
     return ivecs_read(fname).view('float32')
 
 
-
+def bvecs_read(fname):
+    x = np.memmap(fname, dtype='uint8', mode='r')
+    d = x[:4].view('int32')[0]
+    return x.reshape(-1, d + 4)[:, 4:]
